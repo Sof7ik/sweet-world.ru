f8261dbf7ac5d62041f8f4daf5c17aab14bd0147
Leo <bychkov.l47@mail.ru>
2022-03-27 18:39:37 +0300

    fixed directories ierarchy

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="/assets/styles/css/reset.css">
-    <link rel="stylesheet" href="/assets/styles/css/base.css">
-    <link rel="stylesheet" href="/assets/styles/css/header.css">
-    <link rel="stylesheet" href="/assets/styles/css/footer.css">
+    <link rel="stylesheet" href="/src/assets/styles/css/reset.css">
+    <link rel="stylesheet" href="/src/assets/styles/css/base.css">
+    <link rel="stylesheet" href="/src/assets/styles/css/header.css">
+    <link rel="stylesheet" href="/src/assets/styles/css/footer.css">
 
-    <link rel="stylesheet" href="/assets/styles/css/mainpage.css">
+    <link rel="stylesheet" href="/src/assets/styles/css/mainpage.css">
 
-    <script defer src="/assets/js/mainpage-top-slider.js"></script>
+    <script defer src="/src/assets/js/mainpage-top-slider.js"></script>
 </head>
 <body>
     <header id="page-header">
@@ -44,23 +44,23 @@
                 </nav>
 
                 <a href="/" class="logo-wrapper" title="Перейти на главную страницу">
-                    <img src="/upload/images/logo.jpg" alt="Логотип Sweet World">
+                    <img src="/site/upload/images/logo.jpg" alt="Логотип Sweet World">
                 </a>
     
                 <nav class="page-header__right-navigation">
-                    <a href="/personal/" title="Перейти в личный кабинет" class="page-header__site-navigation-link">
+                    <a href="/src/personal/" title="Перейти в личный кабинет" class="page-header__site-navigation-link">
                         <svg width="232" height="232" viewBox="0 0 232 232" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M194.504 173.003C190.23 162.88 184.028 153.684 176.243 145.929C168.482 138.151 159.288 131.95 149.169 127.668C149.078 127.623 148.988 127.6 148.897 127.555C163.012 117.359 172.188 100.752 172.188 82.0156C172.188 50.9766 147.039 25.8281 116 25.8281C84.961 25.8281 59.8126 50.9766 59.8126 82.0156C59.8126 100.752 68.9884 117.359 83.1032 127.577C83.0126 127.623 82.922 127.645 82.8313 127.691C72.6813 131.973 63.5735 138.113 55.7571 145.952C47.9794 153.713 41.7783 162.907 37.4962 173.026C33.2894 182.932 31.0206 193.553 30.8126 204.314C30.8065 204.556 30.8489 204.796 30.9373 205.022C31.0257 205.247 31.1582 205.452 31.3272 205.625C31.4961 205.798 31.6979 205.936 31.9208 206.03C32.1437 206.124 32.3832 206.172 32.6251 206.172H46.2188C47.2157 206.172 48.0087 205.379 48.0313 204.405C48.4845 186.914 55.5079 170.534 67.9235 158.118C80.7696 145.272 97.8298 138.203 116 138.203C134.17 138.203 151.231 145.272 164.077 158.118C176.492 170.534 183.516 186.914 183.969 204.405C183.991 205.402 184.784 206.172 185.781 206.172H199.375C199.617 206.172 199.856 206.124 200.079 206.03C200.302 205.936 200.504 205.798 200.673 205.625C200.842 205.452 200.974 205.247 201.063 205.022C201.151 204.796 201.194 204.556 201.188 204.314C200.961 193.484 198.718 182.949 194.504 173.003V173.003ZM116 120.984C105.601 120.984 95.8134 116.929 88.4501 109.566C81.0868 102.202 77.0313 92.4148 77.0313 82.0156C77.0313 71.6164 81.0868 61.8289 88.4501 54.4656C95.8134 47.1023 105.601 43.0469 116 43.0469C126.399 43.0469 136.187 47.1023 143.55 54.4656C150.913 61.8289 154.969 71.6164 154.969 82.0156C154.969 92.4148 150.913 102.202 143.55 109.566C136.187 116.929 126.399 120.984 116 120.984Z" fill="black"/>
                         </svg>
                     </a>
 
-                    <a href="/cart/" title="Перейти в корзину" class="page-header__site-navigation-link">
+                    <a href="/src/cart/" title="Перейти в корзину" class="page-header__site-navigation-link">
                         <svg width="238" height="238" viewBox="0 0 238 238" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M214.502 163.137H76.0948L83.0442 148.982L198.511 148.773C202.416 148.773 205.763 145.984 206.46 142.126L222.451 52.6203C222.869 50.2729 222.242 47.8557 220.708 46.0195C219.95 45.1159 219.004 44.388 217.936 43.8863C216.869 43.3846 215.705 43.1212 214.525 43.1143L67.6347 42.6262L66.3796 36.7227C65.5894 32.9574 62.196 30.2148 58.3378 30.2148H22.4286C20.2526 30.2148 18.1658 31.0792 16.6272 32.6179C15.0885 34.1565 14.2241 36.2434 14.2241 38.4193C14.2241 40.5953 15.0885 42.6821 16.6272 44.2208C18.1658 45.7594 20.2526 46.6238 22.4286 46.6238H51.6905L57.1757 72.7016L70.6794 138.082L53.2942 166.461C52.3914 167.679 51.8476 169.126 51.7244 170.638C51.6012 172.149 51.9034 173.665 52.597 175.014C53.9915 177.78 56.8038 179.523 59.9183 179.523H74.5144C71.4027 183.656 69.7219 188.69 69.7265 193.863C69.7265 207.018 80.4179 217.71 93.5729 217.71C106.728 217.71 117.419 207.018 117.419 193.863C117.419 188.68 115.7 183.637 112.632 179.523H150.075C146.963 183.656 145.282 188.69 145.287 193.863C145.287 207.018 155.978 217.71 169.133 217.71C182.288 217.71 192.98 207.018 192.98 193.863C192.98 188.68 191.26 183.637 188.192 179.523H214.525C219.034 179.523 222.73 175.85 222.73 171.318C222.716 169.144 221.844 167.064 220.302 165.532C218.761 163.999 216.676 163.138 214.502 163.137V163.137ZM71.0513 58.8027L204.88 59.2443L191.771 132.643L86.6468 132.829L71.0513 58.8027ZM93.5729 201.208C89.5288 201.208 86.2284 197.907 86.2284 193.863C86.2284 189.819 89.5288 186.519 93.5729 186.519C97.6171 186.519 100.917 189.819 100.917 193.863C100.917 195.811 100.144 197.679 98.7663 199.056C97.3889 200.434 95.5208 201.208 93.5729 201.208V201.208ZM169.133 201.208C165.089 201.208 161.789 197.907 161.789 193.863C161.789 189.819 165.089 186.519 169.133 186.519C173.177 186.519 176.478 189.819 176.478 193.863C176.478 195.811 175.704 197.679 174.327 199.056C172.949 200.434 171.081 201.208 169.133 201.208V201.208Z" fill="black"/>
                         </svg>
                     </a>
 
-                    <a href="/liked/" title="Перейти в избранные товары" class="page-header__site-navigation-link">
+                    <a href="/src/liked/" title="Перейти в избранные товары" class="page-header__site-navigation-link">
                         <svg width="250" height="250" viewBox="0 0 250 250" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M225.342 69.2382C222.069 61.661 217.351 54.7946 211.45 49.0234C205.545 43.2349 198.583 38.635 190.942 35.4736C183.019 32.1824 174.522 30.4978 165.942 30.5175C153.906 30.5175 142.163 33.8134 131.958 40.039C129.517 41.5282 127.197 43.164 125 44.9462C122.803 43.164 120.483 41.5282 118.042 40.039C107.837 33.8134 96.0937 30.5175 84.0576 30.5175C75.3906 30.5175 66.9922 32.1777 59.0576 35.4736C51.3916 38.6474 44.4824 43.2128 38.5498 49.0234C32.6415 54.7881 27.9218 61.6561 24.6582 69.2382C21.2646 77.1239 19.5312 85.498 19.5312 94.1161C19.5312 102.246 21.1914 110.718 24.4873 119.336C27.2461 126.538 31.2012 134.009 36.2549 141.553C44.2627 153.491 55.2734 165.942 68.9453 178.564C91.6016 199.487 114.038 213.94 114.99 214.526L120.776 218.237C123.34 219.873 126.636 219.873 129.199 218.237L134.985 214.526C135.938 213.916 158.35 199.487 181.03 178.564C194.702 165.942 205.713 153.491 213.721 141.553C218.774 134.009 222.754 126.538 225.488 119.336C228.784 110.718 230.444 102.246 230.444 94.1161C230.469 85.498 228.735 77.1239 225.342 69.2382V69.2382ZM125 198.926C125 198.926 38.0859 143.237 38.0859 94.1161C38.0859 69.2382 58.667 49.0722 84.0576 49.0722C101.904 49.0722 117.383 59.0331 125 73.5839C132.617 59.0331 148.096 49.0722 165.942 49.0722C191.333 49.0722 211.914 69.2382 211.914 94.1161C211.914 143.237 125 198.926 125 198.926Z" fill="black"/>
                         </svg>
@@ -72,11 +72,11 @@
                 </nav>
 
                 <nav class="page-header__site-navigation">
-                    <a href="/dostavka/" class="site-navigation__link">Доставка</a>
-                    <a href="/o-nas/" class="site-navigation__link">О нас</a>
-                    <a href="/katalog/" class="site-navigation__link">Каталог</a>
-                    <a href="/otzivi/" class="site-navigation__link">Отзывы</a>
-                    <a href="/kontakty/" class="site-navigation__link">Контакты</a>
+                    <a href="/src/dostavka/" class="site-navigation__link">Доставка</a>
+                    <a href="/src/o-nas/" class="site-navigation__link">О нас</a>
+                    <a href="/src/katalog/" class="site-navigation__link">Каталог</a>
+                    <a href="/src/otzivi/" class="site-navigation__link">Отзывы</a>
+                    <a href="/src/kontakty/" class="site-navigation__link">Контакты</a>
                 </nav>
             </div>
         </div>
@@ -87,19 +87,19 @@
             <div class="swiper mainpage-top-slider">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide action-item"
-                         style='background-image: url("/upload/images/mainpage-slider/baanner image.png");'>
+                         style='background-image: url("/site/upload/images/mainpage-slider/baanner image.png");'>
                     </div>
 
                     <div class="swiper-slide action-item"
-                         style="background-image: url('/upload/images/mainpage-slider/banner-choco.png');">
+                         style="background-image: url('/site/upload/images/mainpage-slider/banner-choco.png');">
                     </div>
 
                     <div class="swiper-slide action-item"
-                         style="background-image: url('/upload/images/mainpage-slider/banner-snakes.png');">
+                         style="background-image: url('/site/upload/images/mainpage-slider/banner-snakes.png');">
                     </div>
 
                     <div class="swiper-slide action-item"
-                         style="background-image: url('/upload/images/mainpage-slider/banner-pecenki.png');">
+                         style="background-image: url('/site/upload/images/mainpage-slider/banner-pecenki.png');">
                     </div>
                 </div>
             
@@ -122,7 +122,7 @@
                             <input type="text" name="query" id="mainpage-slider-search">
                         </form>
 
-                        <a href="/katalog/" class="mainpage-top-slider__catalog-btn">Каталог</a>
+                        <a href="/src/katalog/" class="mainpage-top-slider__catalog-btn">Каталог</a>
                     </div>
                 </div>
             </div>
