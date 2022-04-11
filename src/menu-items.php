53008f20830fdc091c187bdd6241f001df97b01f
Leo <bychkov.l47@mail.ru>
2022-04-11 18:14:39 +0300

    refactored hrefs

--- src/menu-items.php
+++ src/menu-items.php
@@ -1,8 +1,8 @@
 <?php
 $arMenuLinks = [
 	"Доставка" => "/src/dostavka/",
-	"О нас" => "/src/o-nas",
-	"Каталог" => "/src/catalog",
-	"Отзывы" => "/src/otzivy",
-	"Контакты" => "/src/contacts",
+	"О нас" => "/src/o-nas/",
+	"Каталог" => "/src/catalog/",
+	"Отзывы" => "/src/otzivy/",
+	"Контакты" => "/src/contacts/",
 ];
