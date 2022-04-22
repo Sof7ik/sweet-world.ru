cbccfa952af23ac13ffd4acdf4316fb3350848a6
Malyshenkova <>
2022-04-22 10:09:28 +0300

    added files about-us

---
+++ src/about-us/index.php
@@ -0,0 +1,18 @@
+<?php
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/src/config.php");
+
+	$APPLICATION["title"] = "Отзывы о нас";
+
+	$APPLICATION["styles"][] = "/src/assets/styles/css/reviews-1.css";
+	$APPLICATION["scripts"][] = "/src/assets/js/reviews.js";
+
+	require_once($_SERVER["DOCUMENT_ROOT"] . "/src/header.php");
+?>
+
+        <div>
+            <h1>О нас</h1>
+        </div>
+
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/src/footer.php");
+?>
